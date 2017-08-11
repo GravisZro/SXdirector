f9f6ef152b9167ed898a3c83a9912d353270f022
Gravis <github@adaptivetime.com>
2017-08-10 23:15:27 -0400

    allow execution as root

--- main.cpp
+++ main.cpp
@@ -31,30 +31,30 @@ int main(int argc, char *argv[]) noexcept
   std::signal(SIGPIPE, SIG_IGN);
 
   posix::syslog.open(appname, posix::facility::daemon);
+  Application app;
 
   if((std::strcmp(posix::getgroupname(::getgid()), groupname) && // if current username is NOT what we want AND
       ::setgid(posix::getgroupid(groupname)) == posix::error_response) || // unable to change user id
      (std::strcmp(posix::getusername(::getuid()), username) && // if current username is NOT what we want AND
       ::setuid(posix::getuserid (username)) == posix::error_response)) // unable to change user id
   {
-    posix::syslog << posix::priority::critical
-                  << "daemon must be launched as username "
+    posix::syslog << posix::priority::error
+                  << "daemon must be launched as user/group "
                   << '"' << username << '"'
                   << " or have permissions to setuid/setgid"
                   << posix::eom;
     std::exit(int(std::errc::permission_denied));
   }
 
-  Application app;
   ExecutorConfigClient config;
 
   if(config.connect(config_path))
   {
-    posix::syslog << posix::priority::info << "Executor connected to " << config_path << posix::eom;
+    posix::syslog << posix::priority::debug << "Executor connected to " << config_path << posix::eom;
   }
   else
   {
-    posix::syslog << posix::priority::info << "Executor unable to connected to " << config_path << posix::eom;
+    posix::syslog << posix::priority::debug << "Executor unable to connected to " << config_path << posix::eom;
   }
 
   return app.exec();
