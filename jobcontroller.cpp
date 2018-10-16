22c3f11ef43897f3def20e0602b47c300f24aedc
GravisZro <github@adaptivetime.com>
2018-10-16 17:56:02 -0400

    add Object::signals and sendSignal()

--- jobcontroller.cpp
+++ jobcontroller.cpp
@@ -10,11 +10,18 @@ void JobController::add(pid_t parent_pid, pid_t child_pid) noexcept
     m_pids.emplace_back(parent_pid, child_pid);
     m_procs.emplace_back(child_pid, ProcessEvent::Fork | ProcessEvent::Exit); // add as child
     Object::connect(m_procs.back().forked, this, &JobController::add);
-    Object::connect(m_procs.back().exited, this, &JobController::remove);
+
+    Object::connect(m_procs.back().exited,
+                    Object::fslot_t<void, pid_t, posix::error_t>([this](pid_t pid, int rval) noexcept
+                      { remove(pid); if(m_procs.empty()) { Object::enqueue(exited, rval); } }));
+
+    Object::connect(m_procs.back().killed,
+                    Object::fslot_t<void, pid_t, posix::signal::EId>([this](pid_t pid, int rval) noexcept
+                      { remove(pid); if(m_procs.empty()) { Object::enqueue(exited, rval); } }));
   }
 }
 
-void JobController::remove(pid_t pid, posix::error_t exit_code) noexcept
+void JobController::remove(pid_t pid) noexcept
 {
   auto iter = m_pids.begin();
   while(iter != m_pids.end())
@@ -33,4 +40,12 @@ void JobController::remove(pid_t pid, posix::error_t exit_code) noexcept
   for(auto piter = m_procs.begin(); piter != m_procs.end(); ++piter) // search all processes
     if(piter->pid() == pid) // if found process with matching PID
       { m_procs.erase(piter); break; } // erase and bail out of loop
+}
+
+bool JobController::sendSignal(posix::signal::EId signum) noexcept
+{
+  bool sent = true;
+  for(auto& proc : m_procs)
+    sent &= posix::signal::send(proc.pid(), signum);
+  return sent;
 }
--- jobcontroller.h
+++ jobcontroller.h
@@ -13,8 +13,13 @@ class JobController : public Object
 public:
   void add(pid_t parent_pid, pid_t child_pid) noexcept;
   const std::list<std::pair<pid_t, pid_t>>& list(void) noexcept { return m_pids; }
+
+  bool sendSignal(posix::signal::EId signum) noexcept;
+
+  signal<posix::error_t> exited; // exit signal with PID and process exit code
+  signal<posix::signal::EId> killed; // killed signal with PID and signal number
 private:
-  void remove(pid_t pid, posix::error_t exit_code) noexcept;
+  void remove(pid_t pid) noexcept;
   std::list<std::pair<pid_t, pid_t>> m_pids;
   std::list<ProcessEvent> m_procs;
 };
