be9a423c4705d24a130d1dfb8429a669d24500af
GravisZro <github@adaptivetime.com>
2018-10-22 00:25:16 -0400

    move shared memory saving/loading and process recog into their own functions

--- directorcore.cpp
+++ directorcore.cpp
@@ -5,6 +5,7 @@
 
 // POSIX++
 #include <climits>
+#include <cstdlib>
 
 // STL
 #include <string>
@@ -17,60 +18,119 @@
 
 // Director
 #include "string_helpers.h"
+#include "exitpending.h"
 
 #include <cassert>
 
-static_assert(sizeof(size_t) == sizeof(std::unordered_map<std::string, JobController>::size_type), "bad size");
-static_assert(sizeof(size_t) == sizeof(std::list<std::pair<pid_t, pid_t>>::size_type), "bad size");
+static_assert(sizeof(size_t) == sizeof(std::unordered_map<int, int>::size_type), "bad size");
+static_assert(sizeof(size_t) == sizeof(std::list<int>::size_type), "bad size");
 
-DirectorCore::DirectorCore(uid_t euid, gid_t egid, posix::fd_t fd_num) noexcept
+DirectorCore::DirectorCore(uid_t euid, gid_t egid, posix::fd_t shmid) noexcept
   : m_euid(euid), m_egid(egid)
 {
-  if(fd_num == posix::invalid_descriptor) // initial run
-  {
-    // search existing processes for those we should be managing (not 100% foolproof)
-    process_state_t state;
-    std::vector<pid_t> pidlist;
-    const pid_t thispid = ::getpid();
-    if(proclist(pidlist) == posix::success_response)
-      for(pid_t pid : pidlist)
-      {
-        std::memset(reinterpret_cast<void*>(&state), 0, sizeof(state));
-        if(procstat(pid, state) == posix::success_response && // if get process state works AND
-           (!state.parent_process_id || state.parent_process_id == thispid)) // process has unknown parent process OR director is the parent process
-          for(const std::string& config : m_director_config_client.listConfigs()) // try each config
-            if(m_director_config_client.get(config, "/Process/Executable") == state.executable) // if the executable matches
-            {
-              m_process_map[config].add(thispid, pid); // claim this as a managed process
-              std::memset(reinterpret_cast<void*>(&state), 0, sizeof(state));
-              for(pid_t childpid : pidlist)
-                if(procstat(childpid, state) == posix::success_response && // if get process state works AND
-                   state.parent_process_id == pid) // process is a child of this parent pid
-                  m_process_map[config].add(pid, childpid); // claim this as a managed process
-            }
-      }
-  }
-  else // reloaded
-  {
-    posix::size_t buffer_size = 0;
-    char* reload_buffer = nullptr;
+  if(!shmLoad(shmid)) // if loading from shared memory failed
+    buildProcessMap(); // rebuild the process map from scratch
 
-    reload_buffer = reinterpret_cast<char*>(::shmat(fd_num, nullptr, 0)); // retrieve shared memory
-    if(reload_buffer == nullptr)
-      posix::syslog << posix::priority::error
-                    << "Unable to allocate shared memory for reload buffer: %1"
-                    << std::strerror(errno)
-                    << posix::eom;
+
+  Object::connect(m_config_client.synchronized, this, &DirectorCore::multiSyncReloadSettings);
+  Object::connect(m_director_config_client.synchronized, this, &DirectorCore::multiSyncReloadSettings);
+}
+
+// prevent m_config_client and m_director_config_client from invoking reloadSettings() multiple times
+void DirectorCore::multiSyncReloadSettings(void) noexcept
+{
+  if(m_synchronized_count < 2) // ensure fully synchronized to avoid multiple reloads
+    ++m_synchronized_count;
+  else
+  {
+    m_synchronized_count = 0;
+    reloadSettings();
+  }
+}
+
+bool DirectorCore::buildProcessMap(void) noexcept
+{
+  // search existing processes for those we should be managing (not 100% foolproof)
+  process_state_t state;
+  std::vector<pid_t> pidlist;
+  const pid_t thispid = ::getpid();
+  if(proclist(pidlist) == posix::success_response)
+  {
+    for(pid_t pid : pidlist)
+    {
+      std::memset(reinterpret_cast<void*>(&state), 0, sizeof(state));
+      if(procstat(pid, state) == posix::success_response && // if get process state works AND
+         (!state.parent_process_id || state.parent_process_id == thispid)) // process has unknown parent process OR director is the parent process
+        for(const std::string& config : m_director_config_client.listConfigs()) // try each config
+          if(m_director_config_client.get(config, "/Process/Executable") == state.executable) // if the executable matches
+          {
+            m_process_map[config].add(thispid, pid); // claim this as a managed process
+            std::memset(reinterpret_cast<void*>(&state), 0, sizeof(state)); // wipe process state for safety
+            for(pid_t childpid : pidlist)
+              if(procstat(childpid, state) == posix::success_response && // if get process state works AND
+                 state.parent_process_id == pid) // process is a child of this parent pid
+                m_process_map[config].add(pid, childpid); // claim this as a managed process
+          }
+    }
+    return true;
+  }
+  return false;
+}
+
+posix::fd_t DirectorCore::shmStore(void) noexcept
+{
+  // buffer size calculation
+  posix::size_t buffer_size = 4 + m_runlevel.size() +
+                              4 + sizeof(size_t);
+  for(auto& pair : m_process_map)
+    buffer_size += 4 + pair.first.size() + // string
+                   4 + sizeof(size_t) + // list size
+                   (4 * pair.second.list().size() * 2 * sizeof(pid_t)); // list of pairs
+  // end buffer size calculation
+
+  posix::fd_t shmid = ::shmget(IPC_PRIVATE, buffer_size, IPC_CREAT | SHM_R | SHM_W); // create shared memory segment
+  if(shmid != posix::invalid_descriptor)
+  {
+    char* reload_buffer = reinterpret_cast<char*>(::shmat(shmid, nullptr, 0)); // retrieve shared memory
+    if(reload_buffer == reinterpret_cast<char*>(-1)) // if error
+    {
+      ::shmctl(shmid, IPC_RMID, nullptr); // release shared memory
+      shmid = posix::invalid_descriptor; // disclaim shared memory id
+    }
     else
     {
-      shmid_ds stat_data;
-      std::memset(&stat_data, 0, sizeof(stat_data));
-      if(::shmctl(fd_num, IPC_STAT, &stat_data) == posix::success_response)
-        buffer_size = stat_data.shm_segsz;
-    }
+      std::memset(reload_buffer, 0, buffer_size); // zero out for safety
+      // save state to shared memory
+      vfifo storage(reload_buffer, posix::ssize_t(buffer_size)); // give it the shared memory buffer
 
-    if(reload_buffer != nullptr && buffer_size)
+      storage << m_runlevel;
+      storage << m_process_map.size();
+      for(auto& pair : m_process_map)
+      {
+        storage << pair.first;
+        auto list = pair.second.list();
+        storage << list.size();
+        for(auto& pair : list)
+          storage << pair.first << pair.second;
+      }
+    }
+  }
+  return shmid;
+}
+
+bool DirectorCore::shmLoad(posix::fd_t shmid) noexcept
+{
+  posix::size_t buffer_size = 0;
+  char* reload_buffer = nullptr;
+
+  reload_buffer = reinterpret_cast<char*>(::shmat(shmid, nullptr, 0)); // retrieve shared memory
+  if(reload_buffer != reinterpret_cast<char*>(-1)) // if not an error
+  {
+    shmid_ds stat_data;
+    std::memset(&stat_data, 0, sizeof(stat_data));
+    if(::shmctl(shmid, IPC_STAT, &stat_data) == posix::success_response)
     {
+      buffer_size = stat_data.shm_segsz;
       std::string name;
       size_t job_count = 0;
       size_t pid_count = 0;
@@ -79,7 +139,7 @@ DirectorCore::DirectorCore(uid_t euid, gid_t egid, posix::fd_t fd_num) noexcept
 
       // load state from memory
       vfifo storage(reload_buffer, posix::ssize_t(buffer_size)); // give it the shared memory buffer
-      storage.expand(posix::ssize_t(buffer_size)); // tell it the buffer is full
+      storage.expand(posix::ssize_t(buffer_size)); // tell "storage" that the buffer is full
 
       storage >> m_runlevel;
       storage >> job_count;
@@ -93,125 +153,23 @@ DirectorCore::DirectorCore(uid_t euid, gid_t egid, posix::fd_t fd_num) noexcept
           proc.add(parent_pid, child_pid);
         }
       }
-      std::memset(reload_buffer, 0, buffer_size); // zero out memory for safety
+      std::memset(reload_buffer, 0, buffer_size); // zero out shared memory for safety
     }
-
-    ::shmctl(fd_num, IPC_RMID, nullptr); // release memory
+    ::shmctl(shmid, IPC_RMID, nullptr); // release shared memory
+    return true;
   }
-
-  Object::connect(m_config_client.synchronized, this, &DirectorCore::reloadSettings);
-  Object::connect(m_director_config_client.synchronized, this, &DirectorCore::reloadSettings);
+  return false;
 }
-
-DirectorCore::~DirectorCore(void) noexcept
-{
-}
-
-const std::string& DirectorCore::getConfigData(const std::string& config, const std::string& key) const noexcept
-{
-  return m_director_config_client.get(config, key);
-}
-
-std::list<std::string> DirectorCore::getConfigList(void) const noexcept
-{
-  return m_director_config_client.listConfigs();
-}
-
-
-DependencySolver::runlevel_t DirectorCore::getRunlevelNumber(const std::string& rlname) const noexcept
-{
-  auto iter = m_runlevel_aliases.find(rlname);
-  if(iter == m_runlevel_aliases.end())
-    return invalid_runlevel;
-  return iter->second;
-}
-
-bool DirectorCore::setRunlevel(const std::string& rlname) noexcept
-{
-  if(!m_action_queue.empty())
-    return false;
-
-  runlevel_t rlnum = getRunlevelNumber(rlname);
-  if(rlnum == invalid_runlevel)
-    return false;
-
-  m_action_queue = getRunlevelOrder(rlname);
-
-  if(rlnum == getRunlevelNumber("bootstrap"))
-  {
-    posix::error_t val = posix::success_response;
-    Object::singleShot(&m_director_config_client, &DirectorConfigClient::resync, val);
-    Object::singleShot(&m_config_client, &ConfigClient::resync, val);
-  }
-
-  m_runlevel = rlname;
-  return true;
-}
-
-void DirectorCore::processJob(void) noexcept
-{
-  if(!m_action_queue.empty())
-  {
-    const std::pair<bool, std::string>& pair = m_action_queue.front();
-    const bool& start = pair.first;
-    const std::string& config = pair.second;
-
-    if(start)
-    {
-      const std::string& executable = m_director_config_client.get(config, "/Process/Executable");
-    }
-    else
-    {
-      auto iter = m_process_map.find(config);
-      if(iter != m_process_map.end())
-      {
-        JobController& job = iter->second;
-        job.sendSignal(decode_signal_name(m_director_config_client.get(config, "/Exiting/Signal")));
-
-        m_director_config_client.get(config, "/Exiting/Timeout");
-        std::set<std::string> services = clean_explode(m_director_config_client.get(config, "/Process/ProvidedServices"), LIST_DELIM);
-
-      }
-    }
-  }
-}
-
 
 void DirectorCore::reloadBinary(void) noexcept
 {
-  // buffer size calculation
-  posix::size_t buffer_size = 4 + m_runlevel.size() +
-                              4 + sizeof(size_t);
-  for(auto& pair : m_process_map)
-    buffer_size += 4 + pair.first.size() + // string
-                   4 + sizeof(size_t) + // list size
-                   (4 * pair.second.list().size() * 2 * sizeof(pid_t)); // list of pairs
-  // end buffer size calculation
-
-  posix::fd_t fd_num = ::shmget(IPC_PRIVATE, buffer_size, IPC_CREAT | SHM_R | SHM_W); // create shared memory segment
-  char* reload_buffer = reinterpret_cast<char*>(::shmat(fd_num, nullptr, 0)); // retrieve shared memory
-  if(reload_buffer == nullptr)
+  posix::fd_t shmid = shmStore();
+  if(shmid == posix::invalid_descriptor)
     posix::syslog << posix::priority::error
                   << "Unable to allocate shared memory for reload buffer: %1"
                   << std::strerror(errno)
                   << posix::eom;
-  else
-  {
-    std::memset(reload_buffer, 0, buffer_size); // zero out for safety
-    // save state to shared memory
-    vfifo storage(reload_buffer, posix::ssize_t(buffer_size)); // give it the shared memory buffer
 
-    storage << m_runlevel;
-    storage << m_process_map.size();
-    for(auto& pair : m_process_map)
-    {
-      storage << pair.first;
-      auto list = pair.second.list();
-      storage << list.size();
-      for(auto& pair : list)
-        storage << pair.first << pair.second;
-    }
-  }
 
   if(!posix::setegid(m_egid) || // unable to change effective group id
      !posix::seteuid(m_euid)) // unable to change effective user id
@@ -226,7 +184,7 @@ void DirectorCore::reloadBinary(void) noexcept
     // reload process
     process_state_t data;
     if(::procstat(::getpid(), data) == posix::success_response)
-      ::execl(data.executable.c_str(), data.executable.c_str(), std::to_string(fd_num).c_str(), NULL);
+      ::execl(data.executable.c_str(), data.executable.c_str(), std::to_string(shmid).c_str(), NULL);
     terminal::write("%s%s\n", terminal::critical, "Failed to reload Director from binary!");
     Application::quit(errno);
   }
@@ -238,7 +196,7 @@ inline bool starts_with(const std::string& str, const char* seek)
 
 void DirectorCore::reloadSettings(void) noexcept
 {
-  if(m_config_client.isSynchronized() &&
+  if(m_config_client.isSynchronized() && // ensure fully synchronized to avoid multiple reloads
      m_director_config_client.isSynchronized())
   {
     // destroy all existing data
@@ -247,6 +205,7 @@ void DirectorCore::reloadSettings(void) noexcept
     m_runlevel_aliases.emplace("bootstrap", -1);
     m_runlevel_aliases.emplace("reboot"   , -2);
     m_runlevel_aliases.emplace("halt"     , -3);
+    m_runlevel_aliases.emplace("poweroff" , -4);
 
     // add custom runlevel aliases
     for(auto& pair : m_config_client.data()) // check every config client entry
@@ -278,13 +237,87 @@ void DirectorCore::reloadSettings(void) noexcept
     }
 
     resolveDependencies();
-  }
 
-  if(m_runlevel.empty()) // runlevel is empty if the director was just just started
-  {
-     setRunlevel("bootstrap"); // switch to the system init runlevel
-     reloadBinary();
+    if(m_runlevel.empty()) // runlevel is empty if the director was just just started
+    {
+      Object::connect(runlevel_changed, Object::fslot_t<void, const std::string>([this](const std::string& rlname) noexcept {
+        terminal::write("oh no: %s\n", rlname.c_str());
+        reloadBinary(); }));
+      setRunlevel("bootstrap"); // switch to the system init runlevel
+    }
+    else if(m_runlevel == "bootstrap")
+      setRunlevel(m_config_client.get("/Settings/InitialRunlevel")); // switch to the initial runlevel
   }
-  else if(m_runlevel == "bootstrap")
-    setRunlevel(m_config_client.get("/Settings/InitialRunlevel")); // switch to the initial runlevel
+}
+
+
+inline const std::string& DirectorCore::getConfigData(const std::string& config, const std::string& key) const noexcept
+{
+  return m_director_config_client.get(config, key);
+}
+
+inline std::list<std::string> DirectorCore::getConfigList(void) const noexcept
+{
+  return m_director_config_client.listConfigs();
+}
+
+inline DependencySolver::runlevel_t DirectorCore::getRunlevelNumber(const std::string& rlname) const noexcept
+{
+  auto iter = m_runlevel_aliases.find(rlname);
+  if(iter == m_runlevel_aliases.end())
+    return invalid_runlevel;
+  return iter->second;
+}
+
+bool DirectorCore::setRunlevel(const std::string& rlname) noexcept
+{
+  if(!m_action_queue.empty()) // if still changing runlevels
+    return false;
+
+  runlevel_t rlnum = getRunlevelNumber(rlname);
+  if(rlnum == invalid_runlevel || // if invalid OR
+     rlnum == getRunlevelNumber(m_runlevel)) // already set
+    return false;
+
+  m_action_queue = getRunlevelOrder(rlname);
+
+  m_runlevel = rlname;
+
+  Object::singleShot(this, &DirectorCore::processJob);
+  return true;
+}
+
+void DirectorCore::processJob(void) noexcept
+{
+  if(m_action_queue.empty())
+  {
+    terminal::write("runlevel is now: '%s'\n", m_runlevel.c_str());
+    Object::enqueue_copy(runlevel_changed, m_runlevel);
+  }
+  else
+  {
+    const std::pair<bool, std::string>& pair = m_action_queue.front();
+    const bool& start = pair.first;
+    const std::string& config = pair.second;
+
+    if(start)
+    {
+      const std::string& executable = m_director_config_client.get(config, "/Process/Executable");
+    }
+    else
+    {
+      auto iter = m_process_map.find(config);
+      if(iter != m_process_map.end())
+      {
+        JobController& job = iter->second;
+        job.sendSignal(decode_signal_name(m_director_config_client.get(config, "/Exiting/Signal")));
+
+        const std::string& timeout = m_director_config_client.get(config, "/Exiting/Timeout");
+        microseconds_t s = std::strtoull(timeout.c_str(), NULL, 10);
+
+        std::set<std::string> services = clean_explode(m_director_config_client.get(config, "/Process/ProvidedServices"), LIST_DELIM);
+
+      }
+    }
+  }
 }
--- directorcore.h
+++ directorcore.h
@@ -20,8 +20,8 @@ class DirectorCore : public Object,
                      public DependencySolver
 {
 public:
-  DirectorCore(uid_t euid, gid_t egid, posix::fd_t fd_num = posix::invalid_descriptor) noexcept; // take shared memory identifier from previous instance
-  virtual ~DirectorCore(void) noexcept;
+  DirectorCore(uid_t euid, gid_t egid, posix::fd_t shmid = posix::invalid_descriptor) noexcept; // take shared memory identifier from previous instance
+  virtual ~DirectorCore(void) noexcept = default;
 
   bool        setRunlevel(const std::string& rlname) noexcept;
   std::string getRunlevel(void) const noexcept { return m_runlevel; }
@@ -30,17 +30,29 @@ public:
   void reloadSettings(void) noexcept;
 
 private:
-  const std::string& getConfigData(const std::string& config, const std::string& key) const noexcept;
-  std::list<std::string> getConfigList(void) const noexcept;
-  runlevel_t getRunlevelNumber(const std::string& rlname) const noexcept;
+// implemented for DependencySolver
+  virtual const std::string& getConfigData(const std::string& config, const std::string& key) const noexcept;
+  virtual std::list<std::string> getConfigList(void) const noexcept;
+  virtual runlevel_t getRunlevelNumber(const std::string& rlname) const noexcept;
+
+// signals
+  signal<const std::string> runlevel_changed;
+
+// functions
+  bool buildProcessMap(void) noexcept;
+  posix::fd_t shmStore(void) noexcept;
+  bool shmLoad(posix::fd_t shmid) noexcept;
   void processJob(void) noexcept;
 
+// variables
   std::string m_runlevel;
   std::map<std::string, runlevel_t> m_runlevel_aliases;
   std::unordered_map<std::string, JobController> m_process_map; // indexed by provider name
 
-  std::queue<std::pair<bool, std::string>> m_action_queue;
+  std::queue<std::pair<bool, std::string>> m_action_queue; // bool (start/stop) + name
 
+  void multiSyncReloadSettings(void) noexcept;
+  uint8_t m_synchronized_count;
   ConfigClient m_config_client;
   DirectorConfigClient m_director_config_client;
   uid_t m_euid;
