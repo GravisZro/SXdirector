179d0041df275b0b8c7234a6c01c0f78d6c427f1
GravisZro <github@adaptivetime.com>
2018-10-25 22:36:47 -0400

    rewrite to be test multiple times and be abstract to test start/stop conditions

--- exitpending.cpp
+++ exitpending.cpp
@@ -7,16 +7,52 @@
 #include <cxxutils/mountpoint_helpers.h>
 #include <specialized/procstat.h>
 
-
-ExitPending::ExitPending(void) noexcept
+EventPending::EventPending(void) noexcept
+  : m_timeout_count(0), m_max_timeout_count(0)
 {
-  Object::connect(m_timer.expired,
-                  Object::fslot_t<void>([this]() noexcept
-                    { Object::enqueue(still_exist() ? timeout : exited); }));
+  Object::connect(m_timer.expired, this, &EventPending::timerExpired);
+}
+
+void EventPending::timerExpired(void) noexcept
+{
+  if(activateTrigger())
+  {
+    Object::enqueue(event_trigger);
+    m_timer.stop();
+  }
+  else if(++m_timeout_count >= m_max_timeout_count) // increment and check if timeout count has been met
+  {
+    Object::enqueue(event_timeout);
+    m_timer.stop();
+  }
+}
+
+// Ensure the timer doesn't check to frequently or infrequently
+bool EventPending::setTimeout(microseconds_t timeout) noexcept
+{
+  if(timeout > seconds(10)) // if timeout is longer than 10 seconds
+  {
+    m_timeout_count = 0;
+    m_max_timeout_count = timeout / seconds(1);
+    timeout = seconds(1);
+  }
+  else if(timeout > seconds(1) / 10) // if timeout is longer than 1/10 of a second
+  {
+    m_timeout_count = 0;
+    m_max_timeout_count = 10;
+    timeout /= 10;
+  }
+  else // if timeout is shorter than 1/10 of a second
+  {
+    m_timeout_count = 0;
+    m_max_timeout_count = 1;
+  }
+
+  return m_timer.start(timeout, timeout);
 }
 
 // test if they exist or not
-bool ExitPending::still_exist(void) noexcept
+bool ExitPending::activateTrigger(void) noexcept
 {
   char path[PATH_MAX];
   if(!m_services.empty())
@@ -27,7 +63,7 @@ bool ExitPending::still_exist(void) noexcept
     {
       std::snprintf(path, sizeof(path), "%s/%s", scfs_path, service.c_str());
       if(posix::access(path, F_OK) == posix::success_response)
-        return true;
+        return false;
     }
   }
   else if(m_pids.empty())
@@ -36,7 +72,22 @@ bool ExitPending::still_exist(void) noexcept
     for(const std::pair<pid_t, pid_t>& pid_pair : m_pids)
       if(procstat(pid_pair.second, state) == posix::success_response &&
          state.state != Zombie)
-        return true;
+        return false;
   }
-  return false;
+  return true;
+}
+
+// test if they exist or not
+bool StartPending::activateTrigger(void) noexcept
+{
+  char path[PATH_MAX];
+  if(scfs_path == nullptr)
+    initialize_paths();
+  for(const std::string& service : m_services)
+  {
+    std::snprintf(path, sizeof(path), "%s/%s", scfs_path, service.c_str());
+    if(posix::access(path, F_OK) != posix::success_response)
+      return false;
+  }
+  return true;
 }
--- exitpending.h
+++ exitpending.h
@@ -10,31 +10,64 @@
 #include <object.h>
 #include <specialized/TimerEvent.h>
 
-class ExitPending : public Object
+
+// helpers
+template<typename T> constexpr microseconds_t seconds(T count) { return 1000000 * count; }
+
+
+class EventPending : public Object
 {
 public:
-  ExitPending(void) noexcept;
+  EventPending(void) noexcept;
+  virtual ~EventPending(void) noexcept = default;
+
+  bool setTimeout(microseconds_t timeout) noexcept;
+
+  signal<> event_timeout;
+  signal<> event_trigger;
+protected:
+  virtual bool activateTrigger(void) noexcept = 0;
+private:
+  void timerExpired(void) noexcept;
+  TimerEvent m_timer;
+  microseconds_t m_timeout_count;
+  microseconds_t m_max_timeout_count;
+};
+
+
+class ExitPending : public EventPending
+{
+public:
+  ExitPending (void) noexcept = default;
   ~ExitPending(void) noexcept = default;
 
-  void clear(void) noexcept
-    { m_services.clear(); m_pids.clear(); m_timer.stop(); }
-
   void setPids(const std::list<std::pair<pid_t, pid_t>>& pids) noexcept
-    { clear(); m_pids = pids; }
+    { m_services.clear(); m_pids = pids; }
 
   void setServices(const std::list<std::string>& services) noexcept
-    { clear(); m_services = services; }
+    { m_pids.clear(); m_services = services; }
 
-  bool setTimeout(microseconds_t timeout) noexcept
-    { return m_timer.start(timeout); }
-
-  signal<> timeout;
-  signal<> exited;
 private:
-  bool still_exist(void) noexcept;
+  bool activateTrigger(void) noexcept;
   std::list<std::pair<pid_t, pid_t>> m_pids;
   std::list<std::string> m_services;
-  TimerEvent m_timer;
 };
+
+
+class StartPending : public EventPending
+{
+public:
+  StartPending (void) noexcept = default;
+  ~StartPending(void) noexcept = default;
+
+  void setServices(const std::list<std::string>& services) noexcept
+    { m_services = services; }
+
+private:
+  bool activateTrigger(void) noexcept;
+  std::list<std::string> m_services;
+};
+
+
 
 #endif // EXITPENDING_H
