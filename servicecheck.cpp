489778a4dd926846232c50ec159c67f91ac23e1f
GravisZro <github@adaptivetime.com>
2018-10-30 02:03:35 -0400

    check if service exists

---
+++ servicecheck.cpp
@@ -0,0 +1,21 @@
+#include "servicecheck.h"
+
+// POSIX++
+#include <climits>
+
+// PDTK
+#include <cxxutils/posix_helpers.h>
+#include <cxxutils/mountpoint_helpers.h>
+
+
+static char path[PATH_MAX];
+
+bool service_exists(const std::string& service)
+{
+  if(scfs_path == nullptr)
+    initialize_paths();
+  std::snprintf(path, sizeof(path), "%s/%s", scfs_path, service.c_str());
+  if(posix::access(path, F_OK) == posix::success_response)
+    return false;
+  return true;
+}
---
+++ servicecheck.h
@@ -0,0 +1,8 @@
+#ifndef SERVICECHECK_H
+#define SERVICECHECK_H
+
+#include <string>
+
+bool service_exists(const std::string& service);
+
+#endif // SERVICECHECK_H
