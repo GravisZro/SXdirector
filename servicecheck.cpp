62dadca793033e0396e1b5e6ab0a15578a77e92e
GravisZro <github@adaptivetime.com>
2018-11-25 10:51:54 -0500

    reinitialize if needed

--- servicecheck.cpp
+++ servicecheck.cpp
@@ -12,8 +12,9 @@ static char path[PATH_MAX];
 
 bool service_exists(const std::string& service)
 {
-  if(scfs_path == nullptr)
-    initialize_paths();
+  if(scfs_path == nullptr &&
+    !reinitialize_paths())
+    return false;
   std::snprintf(path, sizeof(path), "%s/%s", scfs_path, service.c_str());
   if(posix::access(path, F_OK) == posix::success_response)
     return false;
