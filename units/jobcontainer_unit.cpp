555990ba0528f286d0081d5ab95070541b287f02
GravisZro <github@adaptivetime.com>
2019-04-04 02:11:56 -0400

    input is a string

--- units/jobcontainer_unit.cpp
+++ units/jobcontainer_unit.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) noexcept
   std::string arguments;
 
   Application app;
-  JobContainer job(demo);
+  JobContainer job("demo");
 
 
   arguments.append("-s unittest/demo");
