7e1f4e7c99289cb1ed76f2904088ad49f933099c
GravisZro <github@adaptivetime.com>
2019-04-21 16:03:43 -0400

    revamped unit

--- units/process_control_unit.cpp
+++ units/process_control_unit.cpp
@@ -14,33 +14,88 @@
 #include <put/specialized/fileevent.h>
 #include <put/specialized/mountpoints.h>
 
-
-#define UNITTEST_USERNAME  "unittest"
-#define UNITTEST_GROUPNAME UNITTEST_USERNAME
+#include "../string_helpers.h"
 
 #define UNIT_NAME "process_control_unit"
 
+const char* signame(int signum) noexcept
+{
+  static char number[10] = { '\0' };
+  switch(signum)
+  {
+    case SIGABRT: return "SIGABRT";
+    case SIGALRM: return "SIGALRM";
+    case SIGBUS: return "SIGBUS";
+    case SIGCHLD: return "SIGCHLD";
+    case SIGCONT: return "SIGCONT";
+    case SIGFPE: return "SIGFPE";
+    case SIGHUP: return "SIGHUP";
+    case SIGILL: return "SIGILL";
+    case SIGINT: return "SIGINT";
+    case SIGKILL: return "SIGKILL";
+    case SIGPIPE: return "SIGPIPE";
+    case SIGQUIT: return "SIGQUIT";
+    case SIGSEGV: return "SIGSEGV";
+    case SIGSTOP: return "SIGSTOP";
+    case SIGTERM: return "SIGTERM";
+    case SIGTSTP: return "SIGTSTP";
+    case SIGTTIN: return "SIGTTIN";
+    case SIGTTOU: return "SIGTTOU";
+    case SIGUSR1: return "SIGUSR1";
+    case SIGUSR2: return "SIGUSR2";
+    case SIGPOLL: return "SIGPOLL";
+    case SIGPROF: return "SIGPROF";
+    case SIGSYS: return "SIGSYS";
+    case SIGTRAP: return "SIGTRAP";
+    case SIGURG: return "SIGURG";
+    case SIGVTALRM: return "SIGVTALRM";
+    case SIGXCPU: return "SIGXCPU";
+    case SIGXFSZ: return "SIGXFSZ";
+  }
+  posix::sprintf(number, "%i\0", signum);
+  return number;
+}
+
 int main(int argc, char *argv[]) noexcept
 {
-  if(scfs_path == nullptr)
-    reinitialize_paths();
+  if(argc < 3)
+  {
+    terminal::write("%s - %s. Signal or socket name not provided as program arguments.\n", UNIT_NAME, "FAILURE");
+    return EXIT_FAILURE;
+  }
+
+  posix::Signal::EId sigid = decode_signal_name(argv[1]);
+
+  if(posix::strcmp(argv[1], signame(sigid)))
+  {
+    terminal::write("%s - %s. Invalid signal name.\n", UNIT_NAME, "FAILURE");
+    return EXIT_FAILURE;
+  }
+
+  for(int i = 0; i < 32; ++i )
+  {
+    if(i == sigid)
+    {
+      posix::signal(sigid, [](int signum) noexcept
+      {
+        terminal::write("%s - %s.  Signal '%s' caught.  Exiting.\n", UNIT_NAME, "SUCCESS", signame(signum));
+        Application::quit(EXIT_SUCCESS);
+      });
+    }
+    else
+    {
+      posix::signal(i, [](int signum) noexcept
+      {
+        terminal::write("%s - %s.  Incorrect signal '%s' caught.  Exiting.\n", UNIT_NAME, "FAILURE", signame(signum));
+        Application::quit(EXIT_FAILURE);
+      });
+    }
+
+  }
+
   if(scfs_path == nullptr)
   {
     terminal::write("%s - %s: SCFS is not mounted\n", UNIT_NAME, "FAILURE");
-    return EXIT_FAILURE;
-  }
-
-  if(posix::strcmp(posix::getgroupname(posix::getgid()), UNITTEST_GROUPNAME) && // if current groupname is NOT what we want AND
-     !posix::setgid(posix::getgroupid(UNITTEST_GROUPNAME))) // unable to change group id
-  {
-    terminal::write("%s - %s: Unable to change group id to '%s'\n", UNIT_NAME, "FAILURE", UNITTEST_GROUPNAME);
-    return EXIT_FAILURE;
-  }
-
-  if(posix::strcmp(posix::getusername(posix::getuid()), UNITTEST_USERNAME) && // if current username is NOT what we want AND
-     !posix::setuid(posix::getuserid(UNITTEST_USERNAME))) // unable to change user id
-  {
-    terminal::write("%s - %s: Unable to change user id to '%s'\n", UNIT_NAME, "FAILURE", UNITTEST_USERNAME);
     return EXIT_FAILURE;
   }
 
@@ -48,14 +103,13 @@ int main(int argc, char *argv[]) noexcept
   ServerSocket server;
 
   std::string base = scfs_path;
-  base.append("/" UNITTEST_USERNAME "/io");
+  base.append("/").append(argv[2]);
 
   if(!server.bind(base.c_str()))
   {
     terminal::write("%s - %s: Unable to bind test provider to '%s'\n", UNIT_NAME, "FAILURE", base.c_str());
     return EXIT_FAILURE;
   }
-
 
   terminal::write("%s - %s\n", UNIT_NAME, "SUCCESS");
   return app.exec();
