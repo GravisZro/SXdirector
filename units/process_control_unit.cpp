fe6e9e2b5b71e762aa35a2dd4281b920704a3e64
GravisZro <github@adaptivetime.com>
2019-04-21 19:45:41 -0400

    don't assume signal number range

--- units/process_control_unit.cpp
+++ units/process_control_unit.cpp
@@ -18,6 +18,311 @@
 
 #define UNIT_NAME "process_control_unit"
 
+#define SIG_MIN SIGINT
+#define SIG_MAX SIGINT
+
+#if defined(SIGILL)
+# if SIGILL > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGILL
+# endif
+# if SIGILL < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGILL
+# endif
+#endif
+
+#if defined(SIGABRT)
+# if SIGABRT > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGABRT
+# endif
+# if SIGABRT < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGABRT
+# endif
+#endif
+
+#if defined(SIGFPE)
+# if SIGFPE > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGFPE
+# endif
+# if SIGFPE < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGFPE
+# endif
+#endif
+
+#if defined(SIGSEGV)
+# if SIGSEGV > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGSEGV
+# endif
+# if SIGSEGV < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGSEGV
+# endif
+#endif
+
+#if defined(SIGTERM)
+# if SIGTERM > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGTERM
+# endif
+# if SIGTERM < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGTERM
+# endif
+#endif
+
+
+/* Historical signals specified by POSIX. */
+#if defined(SIGHUP)
+# if SIGHUP > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGHUP
+# endif
+# if SIGHUP < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGHUP
+# endif
+#endif
+
+#if defined(SIGQUIT)
+# if SIGQUIT > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGQUIT
+# endif
+# if SIGQUIT < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGQUIT
+# endif
+#endif
+
+#if defined(SIGTRAP)
+# if SIGTRAP > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGTRAP
+# endif
+# if SIGTRAP < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGTRAP
+# endif
+#endif
+
+#if defined(SIGKILL)
+# if SIGKILL > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGKILL
+# endif
+# if SIGKILL < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGKILL
+# endif
+#endif
+
+#if defined(SIGBUS)
+# if SIGBUS > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGBUS
+# endif
+# if SIGBUS < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGBUS
+# endif
+#endif
+
+#if defined(SIGSYS)
+# if SIGSYS > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGSYS
+# endif
+# if SIGSYS < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGSYS
+# endif
+#endif
+
+#if defined(SIGPIPE)
+# if SIGPIPE > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGPIPE
+# endif
+# if SIGPIPE < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGPIPE
+# endif
+#endif
+
+#if defined(SIGALRM)
+# if SIGALRM > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGALRM
+# endif
+# if SIGALRM < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGALRM
+# endif
+#endif
+
+
+/* New(er) POSIX signals (1003.1-2008, 1003.1-2013).  */
+#if defined(SIGURG)
+# if SIGURG > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGURG
+# endif
+# if SIGURG < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGURG
+# endif
+#endif
+
+#if defined(SIGSTOP)
+# if SIGSTOP > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGSTOP
+# endif
+# if SIGSTOP < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGSTOP
+# endif
+#endif
+
+#if defined(SIGTSTP)
+# if SIGTSTP > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGTSTP
+# endif
+# if SIGTSTP < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGTSTP
+# endif
+#endif
+
+#if defined(SIGCONT)
+# if SIGCONT > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGCONT
+# endif
+# if SIGCONT < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGCONT
+# endif
+#endif
+
+#if defined(SIGCHLD)
+# if SIGCHLD > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGCHLD
+# endif
+# if SIGCHLD < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGCHLD
+# endif
+#endif
+
+#if defined(SIGTTIN)
+# if SIGTTIN > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGTTIN
+# endif
+# if SIGTTIN < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGTTIN
+# endif
+#endif
+
+#if defined(SIGTTOU)
+# if SIGTTOU > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGTTOU
+# endif
+# if SIGTTOU < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGTTOU
+# endif
+#endif
+
+#if defined(SIGPOLL)
+# if SIGPOLL > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGPOLL
+# endif
+# if SIGPOLL < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGPOLL
+# endif
+#endif
+
+#if defined(SIGXCPU)
+# if SIGXCPU > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGXCPU
+# endif
+# if SIGXCPU < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGXCPU
+# endif
+#endif
+
+#if defined(SIGXFSZ)
+# if SIGXFSZ > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGXFSZ
+# endif
+# if SIGXFSZ < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGXFSZ
+# endif
+#endif
+
+#if defined(SIGVTALRM)
+# if SIGVTALRM > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGVTALRM
+# endif
+# if SIGVTALRM < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGVTALRM
+# endif
+#endif
+
+#if defined(SIGPROF)
+# if SIGPROF > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGPROF
+# endif
+# if SIGPROF < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGPROF
+# endif
+#endif
+
+#if defined(SIGUSR1)
+# if SIGUSR1 > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGUSR1
+# endif
+# if SIGUSR1 < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGUSR1
+# endif
+#endif
+
+#if defined(SIGUSR2)
+# if SIGUSR2 > SIG_MAX
+#  undef SIG_MAX
+#  define SIG_MAX SIGUSR2
+# endif
+# if SIGUSR2 < SIG_MIN
+#  undef SIG_MIN
+#  define SIG_MIN SIGUSR2
+# endif
+#endif
+
+
 const char* signame(int signum) noexcept
 {
   static char number[10] = { '\0' };
@@ -72,7 +377,7 @@ int main(int argc, char *argv[]) noexcept
     return EXIT_FAILURE;
   }
 
-  for(int i = 0; i < 32; ++i )
+  for(int i = SIG_MIN; i <= SIG_MAX; ++i )
   {
     if(i == sigid)
     {
